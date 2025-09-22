05b8e13907768d0a58dc639e9d1ef1456f64d7ca
Shockingnews <gagemckenzie70@gmail.com>
2025-09-22 10:26:40 -0300

    fixes

--- weaponArray_Challenge/Program.cs
+++ weaponArray_Challenge/Program.cs
@@ -12,26 +12,55 @@ namespace weaponArray_Challenge
     
     internal class Program
     {
+
         static int[] Ammo = { 50, 50, 50, 50 };
         static int[] Chamber = { 15, 3, 10, 1 };
         static void Main(string[] args)
         {
-            Console.WriteLine(Ammo[0]);
-
-            Console.WriteLine(FireWeapon(3));
-
-            if (Chamber[0] == 0)
+            test(50);
+            Console.WriteLine("Ammo: " + Ammo[0]);
+            Console.WriteLine("Clip: " + Chamber[0]);
+            Console.WriteLine("bang bang bang");
+            
+            
+            while(Ammo[0] > 0)
             {
-                Chamber[0] = 15;
-                Ammo[0] = -15;
+                Console.ReadKey();
+                Console.WriteLine("bang bang bang");
+                Console.WriteLine("Ammo: " + Ammo[0]);
+                Console.WriteLine("Clip: " + FireWeapon(3));
+
+                if (Chamber[0] == 0)
+                {
+                    if (Ammo[0] < Chamber[0])
+                    {
+                        Chamber[0] += Ammo[0];
+                        Ammo[0] = 0;
+                        
+                    }
+                    else
+                    {
+                        Chamber[0] += 15;
+                        Ammo[0] -= 15;
+                    }
+                        
+                }
             }
+            
+           
         }
 
-        static int FireWeapon(int Chamber)
+        static void test(int Ammoleft)
         {
-            Chamber = Chamber - 1;
+            int index = Array.IndexOf(Ammo,Ammoleft);
+            Chamber[index] = 15;
+        }
 
-            return Chamber;
+        static int FireWeapon(int fire)
+        {
+            Chamber[0] -= fire;
+          
+            return Chamber[0];
         }
         
     }
